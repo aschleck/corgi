13100b7317d50e586783c37d27f5263e97a111ce
April Schleck <april@exclusivelyducks.com>
2024-05-21 18:14:44 -0700

    Ensure downstream projects pick up our CSS defs

--- build_defs/tailwind.config.mjs
+++ build_defs/tailwind.config.mjs
@@ -3,6 +3,7 @@ import theme from './tailwind.theme.mjs';
 export default {
   content: [
     `bazel-out/*-fastbuild/bin/{java,js}/**/*.{js,ts,jsx,tsx}`,
+    `external/dev_april_corgi~/{java,js}/**/*.{js,ts,jsx,tsx}`,
     `{java,js}/**/*.{js,ts,jsx,tsx}`,
   ],
   theme,
