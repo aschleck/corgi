cd8b1b2b72dad71498fa470039f4eb68d33412b1
April Schleck <april@exclusivelyducks.com>
2024-10-31 13:20:25 -0700

    Fix Jest references to external/

--- build_defs/jest.config.js
+++ build_defs/jest.config.js
@@ -2,13 +2,16 @@ require('process');
 
 /** @type {import('ts-jest').JestConfigWithTsJest} */
 module.exports = {
+  rootDir: process.cwd(),
   testEnvironment: 'jsdom',
   testMatch: ['**/?(*.)+(spec|test).js?(x)'],
   haste: {
     enableSymlinks: true,
   },
   moduleDirectories: [
-    process.cwd(),
     'node_modules',
   ],
+  moduleNameMapper: {
+    '^external/(.*)': '<rootDir>/../$1',
+  },
 };
